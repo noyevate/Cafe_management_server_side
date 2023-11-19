881d78c2aef9902025eaff11b8011a885cf1e55c
Samuel Lucky <101868004+noyevate@users.noreply.github.com>
2023-11-19 08:10:48 +0100

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-
-const app = require("./index")
-const http = require("http");
-const server = http.createServer(app)
-server.listen(process.env.PORT)
-console.log("nodejs server is running @: ", process.env.PORT)
